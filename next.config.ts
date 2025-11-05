24924bda8430c5617e690c684f297a624283e3ff
ahshanul-haquc <ahshanulhaqucgub@gmail.com>
2025-11-05 16:51:30 +0600

    Basic implemented

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['via.placeholder.com','images.pexels.com', 'picsum.photos', 'dummyjson.com','placeimg.com'],
+  },
 };
 
 export default nextConfig;
